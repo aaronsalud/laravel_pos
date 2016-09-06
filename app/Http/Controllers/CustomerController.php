e3162e1656b7c957fe254c2d09efbbfa79a6967a
dwi wijonarko <d.wijonarko@gmail.com>
2016-09-06 20:34:21 +0700

    create customer controller

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Customer;
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $provinces = \App\Province::all();
+        return view('customer.index')->with(['provinces'=>$provinces]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $customer = new Customer();
+        $customer->name=$request->name;
+        $customer->phone=$request->phone;
+        $customer->bbm=$request->bbm;
+        $customer->address=$request->address;
+        $customer->city_id=$request->city_id;
+        $customer->province_id=$request->province_id;
+        $customer->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $customer = Customer::find($id);
+        return response()->json($customer);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $customer = Customer::find($id);
+        $customer->name=$request->name;
+        $customer->phone=$request->phone;
+        $customer->bbm=$request->bbm;
+        $customer->address=$request->address;
+        $customer->city_id=$request->city_id;
+        $customer->province_id=$request->province_id;
+        $customer->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $customer = Customer::find($id);
+        $customer->delete();
+    }
+}

54af8c1da1d9b30f9e9c0f93e26410ca21dffac9
dwi wijonarko <d.wijonarko@gmail.com>
2016-09-06 20:30:06 +0700

    add migration customer table

---
+++ database/migrations/2016_09_06_132154_create_costumer_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCostumerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('phone');
+            $table->string('bbm');
+            $table->string('address');
+            $table->integer('city_id');
+            $table->integer('province_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('customers');
+    }
+}

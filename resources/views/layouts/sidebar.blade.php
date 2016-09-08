d2c171a5f4329b556aef1354a915c3208ed08afd
dwi wijonarko <d.wijonarko@gmail.com>
2016-09-08 11:53:51 +0700

    sidebar menu done

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -19,8 +19,24 @@
 
            </ul>
         </li>
+        <li >
+            <a href="#" class=" hvr-bounce-to-right"><i class="fa fa-money nav_icon"></i> <span class="nav-label">Transaksi</span><span class="fa arrow"></span></a>
+            <ul class="nav nav-second-level">
+                <li><a href="{{ url('purchase')}}" class=" hvr-bounce-to-right"> <i class="fa fa-shopping-basket nav_icon @{{activepurchase}}"></i>Pembelian</a></li>
+                
+                <li><a href="{{url('sale')}}" class=" hvr-bounce-to-right"><i class="fa fa-credit-card nav_icon @{{activeitem}}"></i>Penjualan</a></li>
+           </ul>
+        </li>
+        <li >
+            <a href="#" class=" hvr-bounce-to-right"><i class="fa fa-area-chart nav_icon"></i> <span class="nav-label">Laporan</span><span class="fa arrow"></span></a>
+            <ul class="nav nav-second-level">
+                <li><a href="{{ url('purchase/report')}}" class=" hvr-bounce-to-right"> <i class="fa fa-line-chart nav_icon @{{activepurchase}}"></i>Pembelian</a></li>
+                
+                <li><a href="{{url('sale/report')}}" class=" hvr-bounce-to-right"><i class="fa fa-bar-chart nav_icon @{{activeitem}}"></i>Penjualan</a></li>
+           </ul>
+        </li>
          <li>
-            <a href="inbox.html" class=" hvr-bounce-to-right"><i class="fa fa-inbox nav_icon"></i> <span class="nav-label">Inbox</span> </a>
+            <a href="inbox.html" class=" hvr-bounce-to-right"><i class="fa fa-gear nav_icon"></i> <span class="nav-label">Settings</span> </a>
         </li>
     </ul>
 </div>
